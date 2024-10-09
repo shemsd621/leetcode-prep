73c422120fc3747eb13e683e247826a0ae72f6b5
Shemaya Sandanayaka <86514942+shemsd621@users.noreply.github.com>
2024-10-09 23:50:07 +0530

    Add ContainerWithMostWater

--- src/Arrays/ContainerWithMostWater.java
+++ src/Arrays/ContainerWithMostWater.java
@@ -23,6 +23,7 @@ package Arrays;
  * 0 <= height[i] <= 104
  */
 
+// time complexity - O(n)
 public class ContainerWithMostWater
 {
 
@@ -30,21 +31,42 @@ public class ContainerWithMostWater
     {
 
         int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println("The maximum amount of water the container can store is: " + maxArea(height));
 
     }
 
-    public void maxArea(int[] height)
+    /*
+    Method to calculate the maximum amount of water a container can store
+     */
+    public static int maxArea(int[] height)
     {
 
-        int n = height.length;
-        int left = 0;
-        int right = n - 1;
-        int maxArea = 0;
+        int n = height.length;  // length of the array height
+        int left = 0;           // left pointer
+        int right = n - 1;      // right pointer
+        int maxArea = 0;        // maximum area
 
+        // loops until the pointers meet
         while (left < right)
         {
+            // calculates the current area
+            // gets the minimum of the left and right values of the array (height) and multiplies with their difference (width)
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            // updates MaxArea if currentArea is larger than maxArea
+            maxArea = Math.max(maxArea, currentArea);
 
+            // updates the position of the pointers
+            if (height[left] < height[right])
+            {
+                // moves the left pointer to the right
+                left++;
+            } else
+            {
+                // moves the right pointer to the left
+                right--;
+            }
         }
+        return maxArea;
 
     }
 
