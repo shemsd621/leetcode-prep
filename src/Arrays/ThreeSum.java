cd850851c82ed47aa226a21304505febb0bfcf0d
Shemaya Sandanayaka <86514942+shemsd621@users.noreply.github.com>
2024-10-10 23:20:32 +0530

    Create ThreeSum.java

---
+++ src/Arrays/ThreeSum.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+import java.util.List;
+
+/**
+ * TAGS
+ * array | two-pointers
+ *
+ * COMPANIES
+ * adobe | amazon | bloomberg | facebook | microsoft
+ *
+ * QUESTION
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * CONSTRAINTS:
+ *
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ */
+
+public class ThreeSum
+{
+
+    public static void main(String[] args)
+    {
+        int[] nums = {-1,0,1,2,-1,-4};
+    }
+
+    public List<List<Integer>> threeSum(int[] nums)
+    {
+        
+    }
+
+}
