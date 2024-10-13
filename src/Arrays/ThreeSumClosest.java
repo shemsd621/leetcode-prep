8e6c481c4342234a4399797d6cc586b873956eeb
Shemaya Sandanayaka <86514942+shemsd621@users.noreply.github.com>
2024-10-13 13:15:56 +0530

    Create ThreeSumClosest.java

---
+++ src/Arrays/ThreeSumClosest.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+/**
+ * TAGS
+ * array | two-pointers
+ *
+ * COMPANIES
+ * bloomberg
+ *
+ * QUESTION
+ * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ *
+ * Return the sum of the three integers.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ * CONSTRAINTS:
+ *
+ * 3 <= nums.length <= 500
+ * -1000 <= nums[i] <= 1000
+ * -104 <= target <= 104
+ */
+
+public class ThreeSumClosest {
+}
